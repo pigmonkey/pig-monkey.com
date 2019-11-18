936d93338041ef0c0c36c4a3c13fca367fe6c926
Pig Monkey <pm@pig-monkey.com>
2019-11-17 21:59:01 -0800

    get csvjson from path

--- books.sh
+++ books.sh
@@ -6,6 +6,6 @@ calibredb catalog content/media/library/books.csv --fields=id,author_sort,title,
 
 LANG=C sed -i '1 s/[\d128-\d255]//g' content/media/library/books.csv
 
-~/.virtualenvs/csvkit/bin/csvjson -i 4 content/media/library/books.csv > content/media/library/books.json
+csvjson -i 4 content/media/library/books.csv > content/media/library/books.json
 
 cd "$OLDPWD"
