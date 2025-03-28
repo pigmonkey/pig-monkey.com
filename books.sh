83d54faa73fe0c237ae33c8da2bbc9848a8f7334
Pig Monkey <pm@pig-monkey.com>
2025-03-28 14:48:17 -0700

    exclude news from read books list

--- books.sh
+++ books.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-calibredb catalog content/media/library/books.csv --fields=id,author_sort,title,isbn,identifiers,series,series_index,uuid --search="#read:yes"
+calibredb catalog content/media/library/books.csv --fields=id,author_sort,title,isbn,identifiers,series,series_index,uuid --search="#read:yes not tag:=news"
 
 LANG=C sed -i '1 s/[\d128-\d255]//g' content/media/library/books.csv
 
